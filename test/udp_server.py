7ee8611faf514bac97a8d6160f9041f187d38d22
Administrator <admin@example.com>
2021-03-12 17:04:20 +0800

    增加连接数

--- test/udp_server.py
+++ test/udp_server.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
                 data_bytes, addr = sock_server.recvfrom(1024)
                 print(data_bytes)
                 print(addr)
-                sss = b'66776c7a-c8f4-4143-bfac-b9813fdec4ef|192.168.43.181|0100|SETUP|1|190|\n'
+                sss = b'66776c7a-c8f4-4143-bfac-b9813fdec4ef|192.168.43.181|0100|ACK|1|15|\n'
                 sock_server.sendto(sss,addr)
     except Exception as e:
         print("[Data_Link]数据回传链路接收数据时发生异常...")
