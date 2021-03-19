fde36dcc9133b429a9315e63979c385b61c423d0
Administrator <admin@example.com>
2021-03-19 17:29:15 +0800

    增加连接数

--- common/UDPServer.py
+++ common/UDPServer.py
@@ -6,6 +6,7 @@ UDPServer, 通信服务
 服务端口：9527（UDP）
 '''
 import socket
+from common.udp_data_handle import *
 
 def working(_1553b):
     print("[UDPServer]打开通信服务...")
@@ -15,13 +16,42 @@ def working(_1553b):
 
         while True:
             data_bytes,addr = sock_server.recvfrom(1024)
-            transaction(data_bytes,addr)
+            transaction(data_bytes,addr,_1553b)
     except Exception as e:
+        print(e)
         print("[UDPServer]发生通信异常...")
     finally:
         sock_server.close()
         print("[UDPServer]通信服务已关闭...")
 
 #处理来自终端的数据包
-def transaction(data_bytes,addr):
-    pass
+def transaction(data_bytes,addr,_1553b):
+    try:
+        data = data_bytes.decode("utf-8")
+        if data.endswith("\n") != True:  #如果不是以'\n'结尾就丢弃，然后退出
+            return
+
+        data = data.split('|')  #根据device_type交给不同的处理方法
+
+        if data[2] == '0100':
+            handle_0100(data,addr,_1553b)
+        elif data[2] == '0200':
+            print(1)
+        elif data[2] == '0300':
+            print(1)
+        else:
+            print("设备类型不对") #设备类型不对，丢弃
+            return
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    data = "deviceuuid|localip|0100|doit|onoff|period|\n"
+    data = data.split('|')
+    if data[2] == '0100':
+        print(data)
+    if data[2] == '0200':
+        print(2)
+    if data[2] == '0300':
+        print(13)
