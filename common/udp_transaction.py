321cb8304a745bf7e25d16fabc333f16d1b6e9c7
Administrator <admin@example.com>
2021-03-19 19:23:48 +0800

    增加连接数

--- common/udp_transaction.py
+++ common/udp_transaction.py
@@ -7,8 +7,8 @@ import datetime
 
 #处理消息[设备0100]
 #"deviceuuid|localip|devicetype|doit|onoff|period|\n";
-def handle_0100(data,addr,_1553b):
-    if data[3] == 'SYN': #设备上线报文
+def transaction_0100(data,addr,_1553b):
+    if data[3] == 'SYN': #设备上线报文处理
         device = DEVICES.query.filter_by(device_id=data[0], device_type=data[2]).first()
         #print(device.config)
         if device is None:  #如果设备不存在，即自动注册
@@ -21,12 +21,13 @@ def handle_0100(data,addr,_1553b):
             period = eval(device.config).get('period')
         except Exception:
             period = 300
-        s = device.device_id + "|" + device.device_ip + "|" + device.device_type + "|" + "ACK" + "|" + str(device.onoff) + "|" + str(period) + "|\n"
+        data_resp = device.device_id + "|" + device.device_ip + "|" + device.device_type + "|" + "ACK" + "|" + str(device.onoff) + "|" + str(period) + "|\n"
 
         sock_c = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock_c.sendto(s.encode("utf-8"), addr)
+        sock_c.sendto(data_resp.encode("utf-8"), addr)
 
     elif data[3] == 'ACTIVE':
+        #db_session.query(User).filter(User.id == 179074001).update({"name":"XXX"})
         pass
     else:
         return
