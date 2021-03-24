fae607c5045ae54210c40bc3976991dd7330e573
Administrator <admin@example.com>
2021-03-24 17:28:48 +0800

    增加连接数

--- common/TCPServer.py
+++ common/TCPServer.py
@@ -6,6 +6,8 @@ TCPServer, 文件传输服务
 服务端口：9528（TCP）
 '''
 import socket
+import uuid
+import threading
 
 def working(_1553b):
     print("[TCPServer]打开文件传输服务...")
@@ -15,11 +17,12 @@ def working(_1553b):
 
         sock_server.listen()
         while True:
-            sock, addr = sock_server.accept()
-            t = threading.Thread(target=send_command, args=(sock, addr))
+            client_socket, client_addr = sock_server.accept()
+            t = threading.Thread(target=getPic, args=(client_socket, client_addr))
             t.setDaemon(True)
             t.start()
     except Exception as e:
+        print(e)
         print("[TCPServer]发生通信异常...")
     finally:
         sock_server.close()
@@ -27,8 +30,38 @@ def working(_1553b):
 
 #控制链路服务器端
 #所有命令格式为“命令字串”
-def send_command(sock, addr):
-    pass
+def getPic(client_socket, client_addr):
+    picpath = 'C:/' + str(uuid.uuid1()) + ".jpg"
+    recv_len = recv_file_head(client_socket, 6)
+    print('应接收到的数据为:', recv_len)
+    recv_picture(client_socket, recv_len, picpath)
 
-def Transaction(data_bytes,addr):
-    pass
+#计算图片的字节数
+def recv_file_head(socket, headsize):
+    recv_data = socket.recv(headsize)
+    recv_len = 0
+
+    if len(recv_data) != headsize:
+        return 0
+
+    print(recv_data[0], recv_data[1])
+    if recv_data[0] == 143 and recv_data[1] == 141:
+        recv_len = recv_data[2] * 16777216 + recv_data[3] * 65536 + recv_data[4] * 256 + recv_data[5]
+
+    return recv_len
+
+#接收图片
+def recv_picture(socket, size, path):
+    # recv_data = 0
+    count = 0
+    # wb以二进制方式写文件,因为我们的tcp接收数据为二进制byte的类型
+    file = open(path, 'wb')
+    while True:
+        recv_data = socket.recv(size)
+        print(count, len(recv_data))
+        if len(recv_data) != 0:
+            count += len(recv_data)
+            file.write(recv_data)
+        else:
+            file.close()
+            break
